be48a381e0d96805e641b4b2d206aaec378bbf2d
Moroi <4635854+Rango-dz@users.noreply.github.com>
2022-07-23 16:16:48 +0100

    Update _app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,13 +1,17 @@
 import '../index.css';
 import '../global.css';
+import dynamic from 'next/dynamic'
 import Layout from '../src/components/Layout';
 
+const DynamicHeader = dynamic(() => import('../components/header'), {
+  suspense: true,
+})
 
 export default function MyApp({ Component, pageProps }) {
 
   return (
-    <Layout>
+    <DynamicHeader>
       <Component {...pageProps} />
-    </Layout>
+    </DynamicHeader>
   );
 }
