22237500b13654a74264621ff84a9f0c0146dd40
Moroi <4635854+Rango-dz@users.noreply.github.com>
2022-07-23 18:45:50 +0100

    Menu shift

--- web/src/components/Header/headerMiddle.js
+++ web/src/components/Header/headerMiddle.js
@@ -62,8 +62,8 @@ export default function HeaderMiddle(props) {
                   </Link>
 
 
-                  <Link href="/doctor" >
-                    <a className={router.pathname == "/doctor" ? "flex self-center uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-moroi-pinkdanger text-colorTwo dark:text-colorRed transition duration-300 font-semibold leading-5 space-x-1 text-sm" : "flex self-center uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-colorRed transition duration-300 font-semibold leading-5 space-x-1 text-sm"}><FaMedkit className=' text-iconcolor dark:text-colorEight hover:text-iconcolorhover m-[0.2rem] w-4' />Doctors</a>
+                  <Link href="/about" >
+                    <a className={router.pathname == "/doctor" ? "flex self-center uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-moroi-pinkdanger text-colorTwo dark:text-colorRed transition duration-300 font-semibold leading-5 space-x-1 text-sm" : "flex self-center uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-colorRed transition duration-300 font-semibold leading-5 space-x-1 text-sm"}><FaMedkit className=' text-iconcolor dark:text-colorEight hover:text-iconcolorhover m-[0.2rem] w-4' />About us</a>
                   </Link>
 
                   <Link href="/article" >
@@ -98,9 +98,9 @@ export default function HeaderMiddle(props) {
                 <a className={router.pathname == "/" ? "flex self-start uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-moroi-pinkdanger text-colorTwo dark:text-colorRed transition duration-300 font-semibold leading-5 space-x-1 text-sm bg-colorSix w-full hover:bg-colorSix dark:bg-moroi-dark dark:hover:bg-moroi-dark rounded" : "hover:bg-colorSix dark:hover:bg-moroi-dark w-full flex self-start uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-moroi-pinkdanger transition duration-300 font-semibold leading-5 space-x-1 text-sm rounded"}><FaHome className={router.pathname == "/contact" ? "text-iconcolorhover dark:text-colorRed m-[0.2rem] w-4 focus:text-colorTwo dark:focus:text-colorRed" : "text-iconcolor dark:text-colorEight hover:text-iconcolorhover dark:hover:text-colorEight m-[0.2rem] w-4 focus:text-colorTwo"} />Home</a>
               </Link>
 
-              <Link href="/doctor" >
+              <Link href="/about" >
 
-                <a className={router.pathname == "/doctor" ? "flex self-start uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-moroi-pinkdanger text-colorTwo dark:text-colorRed transition duration-300 font-semibold leading-5 space-x-1 text-sm bg-colorSix w-full hover:bg-colorSix dark:bg-moroi-dark dark:hover:bg-moroi-dark rounded" : "hover:bg-colorSix dark:hover:bg-moroi-dark w-full flex self-start uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-moroi-pinkdanger transition duration-300 font-semibold leading-5 space-x-1 text-sm rounded"}><FaMedkit className={router.pathname == "/contact" ? "text-iconcolorhover dark:text-colorRed m-[0.2rem] w-4 focus:text-colorTwo dark:focus:text-colorRed" : "text-iconcolor dark:text-colorEight hover:text-iconcolorhover dark:hover:text-colorEight m-[0.2rem] w-4 focus:text-colorTwo"} />Doctors</a>
+                <a className={router.pathname == "/doctor" ? "flex self-start uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-moroi-pinkdanger text-colorTwo dark:text-colorRed transition duration-300 font-semibold leading-5 space-x-1 text-sm bg-colorSix w-full hover:bg-colorSix dark:bg-moroi-dark dark:hover:bg-moroi-dark rounded" : "hover:bg-colorSix dark:hover:bg-moroi-dark w-full flex self-start uppercase px-2 py-2 hover:text-colorTwo dark:hover:text-moroi-pinkdanger transition duration-300 font-semibold leading-5 space-x-1 text-sm rounded"}><FaMedkit className={router.pathname == "/contact" ? "text-iconcolorhover dark:text-colorRed m-[0.2rem] w-4 focus:text-colorTwo dark:focus:text-colorRed" : "text-iconcolor dark:text-colorEight hover:text-iconcolorhover dark:hover:text-colorEight m-[0.2rem] w-4 focus:text-colorTwo"} />About us</a>
               </Link>
 
               <Link href="/article" >
