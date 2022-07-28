cb9f0e6a85895043d566e4b300727fc3071cf98c
Moroi <4635854+Rango-dz@users.noreply.github.com>
2022-07-28 17:03:16 +0100

    Update api.js

--- web/lib/api.js
+++ web/lib/api.js
@@ -69,6 +69,10 @@ _id},
         HomeSubtitle,
         Hometext,
   },
+  "doctorSettings":* [_type == "TheDoctor"]{
+    "image":mainImage{ asset -> { url } },
+...
+  },
 }`
   );
   return response;
