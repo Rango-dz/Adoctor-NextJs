b406ffa538c117c6a5ed4499a689c3232bb517dd
Moroi <4635854+Rango-dz@users.noreply.github.com>
2022-08-25 18:56:15 +0100

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -4,9 +4,11 @@ const prod = process.env.NODE_ENV === 'production'
 const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self';
-  child-src ${process.env.NEXT_PUBLIC_BASE_URL};
-  style-src 'self' ${process.env.NEXT_PUBLIC_BASE_URL};
-  font-src 'self';  
+  child-src 'self' ${process.env.NEXT_PUBLIC_BASE_URL} data:;
+  style-src 'self' https://fonts.googleapis.com 'unsafe-inline' ${process.env.NEXT_PUBLIC_BASE_URL} data:;
+  font-src 'self' https://fonts.gstatic.com; 
+  img-src 'self' https://cdn.sanity.io data:;
+  manifest-src 'self',
 `
 
 const STUDIO_REWRITE = {
@@ -41,6 +43,30 @@ module.exports = withPWA({
           {
             key: 'Content-Security-Policy',
             value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
           }
         ],
       },
