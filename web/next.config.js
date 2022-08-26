920f7a89ba65682767ea64368740e50b593adeb8
Moroi <4635854+Rango-dz@users.noreply.github.com>
2022-08-26 18:50:32 +0100

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -3,11 +3,11 @@ const prod = process.env.NODE_ENV === 'production'
 
 const ContentSecurityPolicy = `
   default-src 'self' ${process.env.NEXT_PUBLIC_BASE_URL} data: https://*.sanity.io  https://*.googleapis.com https://*.vercel-insights.com https://*.algolia.net https://*.algolianet.com https://*.algolia.io https://*.vercel.app https://*.googleapis.com;
-  script-src 'self' ${process.env.NEXT_PUBLIC_BASE_URL} data: https://*.sanity.io  https://*.googleapis.com https://*.vercel-insights.com https://*.algolia.net https://*.algolianet.com https://*.algolia.io https://*.vercel.app https://*.googleapis.com;
+  script-src 'self' ${process.env.NEXT_PUBLIC_BASE_URL} data: https://*.sanity.io  https://*.googleapis.com https://*.vercel-insights.com https://*.algolia.net https://*.algolianet.com https://*.algolia.io https://*.vercel.app https://*.googleapis.com 'sha256-XXX';
   child-src 'self' ${process.env.NEXT_PUBLIC_BASE_URL} data: https://*.sanity.io  https://*.googleapis.com https://*.vercel-insights.com https://*.algolia.net https://*.algolianet.com https://*.algolia.io https://*.vercel.app https://*.googleapis.com;
   style-src 'self' https://*.googleapis.com 'unsafe-inline' ${process.env.NEXT_PUBLIC_BASE_URL} data: https://*.googleapis.com https://*.sanity.io  https://*.googleapis.com https://*.vercel-insights.com https://*.algolia.net https://*.algolianet.com https://*.algolia.io https://*.vercel.app https://*.googleapis.com;
   font-src 'self' https://*.gstatic.com data: https://*.googleapis.com; 
-  img-src 'self' https://*.sanity.io data: https://*.googleapis.com;
+  img-src 'self' https://*.sanity.io data: https://*.googleapis.com blob:;
   manifest-src 'self',
 `
 
