267da0c5ae88a246fc71b2d1a81e239533339f33
Moroi <4635854+Rango-dz@users.noreply.github.com>
2022-08-27 11:46:08 +0100

    Update userMenu.js

--- web/src/components/Header/userMenu.js
+++ web/src/components/Header/userMenu.js
@@ -13,7 +13,7 @@ function userMenu(props) {
         <ul className="dropdown-menu absolute hidden text-gray-700 pt-1 -left-28">
           <li className="rounded-t bg-gray-200  py-2 px-4 block whitespace-no-wrap">{user.name}</li>
           <li className="bg-gray-200  py-2 px-4 block whitespace-no-wrap">{user.email}</li>
-          <li className=""><a className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap cursor-pointer" onClick={() => retour.push('/api/auth/logout')}>Logout</a></li>
+          <li className=""><button className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap cursor-pointer" onClick={() => retour.push('/api/auth/logout')}>Logout</button></li>
         </ul>
       </div>
 
